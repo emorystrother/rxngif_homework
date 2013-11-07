79928ba79d466d30f5c2b376468e26ad07788194
Emory Strother <estrother2014@kellogg.northwestern.edu>
2013-11-07 13:16:53 -0600

    Hwk Update

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,30 @@
 class PicturesController < ApplicationController
+
   def show
+    @picture = Picture.find params[:id]
+  end
+
+  def index
+    @picture_index = Picture.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @picture = Picture.new
+    @picture.source = params[:source]
+    @picture.caption = params[:caption]
+    @picture.save
+  end
+
+  def destroy
+    @picture_index = Picture.all
+  end
+
+  def dead
+    @picture = Picture.find_by id = params[:id]
+    # @picture.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/picture_all", { :controller => "pictures", :action => "index" })
+  get("new", { :controller => "pictures", :action => "new" })
+  get("/create/", { :controller => "pictures", :action => "create" })
+  get("/destroy/", { :controller => "pictures", :action => "destroy" })
+  get("/dead/", { :controller => "pictures", :action => "dead" })
 end
+
